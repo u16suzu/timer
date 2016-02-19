3f29306650a8d1a86d6c1ea467c960d12a54976d
u16suzu <u16suzu@gmail.com>
2016-02-20 03:43:56 +0900

    add options

--- timer.rb
+++ timer.rb
@@ -1,7 +1,14 @@
 
 class Timer
   def self.input_in_sec
-    ARGV[0].to_i # * 60
+    case ARGV[0].to_s
+    when "-m"
+      ARGV[1].to_i
+    when "-s"
+      ARGV[1].to_i * 60
+    else
+      ARGV[0].to_i * 60
+    end
   end
 
   def wait( second )
