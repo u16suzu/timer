e83a05edf0d74025eee514aa83a5c1645c052b9f
u16suzu <u16suzu@gmail.com>
2016-02-20 03:28:39 +0900

    show remain time

--- main.rb
+++ main.rb
@@ -1,6 +1,14 @@
 # 分単位で入力する
 require "terminal-notifier"
 
-sleep ARGV[0].to_i # * 60
+def sec
+  ARGV[0].to_i # * 60
+end
+
+(0..sec).each do |i|
+  print "\r#{i}sec  "
+  sleep 1
+end
+
 puts "Timer finish!"
 TerminalNotifier.notify( 'Timer finish!', sound: 'Hero' )
