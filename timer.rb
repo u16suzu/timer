cfc990315cef2c7da494c5fa2b7af08cfb8b091e
u16suzu <u16suzu@gmail.com>
2016-02-21 23:27:32 +0900

    fix bug : -s と -m オプションの動作が逆だった

--- timer.rb
+++ timer.rb
@@ -6,9 +6,9 @@ class Timer
   def self.input_in_sec
     case ARGV[0].to_s
     when '-m'
-      ARGV[1].to_i
-    when '-s'
       ARGV[1].to_i * 60
+    when '-s'
+      ARGV[1].to_i
     else
       ARGV[0].to_i * 60
     end
